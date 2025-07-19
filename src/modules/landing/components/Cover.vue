5120541c097d9783cc0420b3a9b16fd134410128
ViperBite03 <layitamartin@gmail.com>
2025-07-19 17:25:27 +0200

    churro

---
+++ src/modules/landing/components/Cover.vue
@@ -0,0 +1,67 @@
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+  .cover-container {
+    position: relative;
+    width: 100%;
+    background-color: #000;
+
+    .logo {
+      position: absolute;
+
+      margin: auto;
+      top: 0;
+      bottom: 0;
+      left: 0;
+      right: 0;
+
+      //height: 75%;
+      z-index: 1;
+    }
+
+    .section {
+      position: relative;
+      width: 100%;
+      height: 100vh;
+      overflow: hidden;
+
+      #myVideo {
+        width: 100%;
+        position: absolute;
+        opacity: 0.5;
+      }
+
+      &.s2 {
+        filter: blur(100px);
+      }
+    }
+
+    .splitter {
+      width: 100%;
+      margin-top: -25px;
+      z-index: 99;
+      position: relative;
+      box-shadow: 0px 0px 120px 100px black;
+    }
+  }
+</style>
+
+<template>
+  <div class="cover-container">
+    <!--<img src="unicorn.png" alt="logo" class="logo" />-->
+
+    <div class="section s1">
+      <video autoplay muted loop id="myVideo">
+        <source src="forest.mp4" type="video/mp4" />
+      </video>
+    </div>
+
+    <div class="splitter"></div>
+
+    <div class="section s2">
+      <video autoplay muted loop id="myVideo">
+        <source src="forest.mp4" type="video/mp4" />
+      </video>
+    </div>
+  </div>
+</template>
--- src/modules/landing/components/Menu.vue
+++ src/modules/landing/components/Menu.vue
@@ -1,9 +1,65 @@
 <script setup lang="ts">
-  import { svgStore } from '@/modules/shared/constants/svgStore'
+  import { ref, onMounted, onUnmounted } from 'vue'
+
+  const items1 = ['Apartments', 'Massage salon']
+  const items2 = ['Attractions', 'About us']
+
+  const hasScrolled = ref(false)
+
+  const checkScroll = () => {
+    hasScrolled.value = window.scrollY > 0
+  }
+
+  onMounted(() => {
+    checkScroll()
+    window.addEventListener('scroll', checkScroll)
+  })
+
+  onUnmounted(() => {
+    window.removeEventListener('scroll', checkScroll)
+  })
 </script>
 
-<style scoped lang="scss"></style>
+<style lang="scss" scoped>
+  #menu-container {
+    transition: 0.3s ease;
+    width: 100%;
+    position: fixed;
+    display: grid;
 
-<template>
-  <div class="menu">hola</div>
-</template>
+    grid-template-columns: 1fr 1fr 1fr;
+    align-items: center;
+    justify-items: center;
+
+    padding: 25px 0px;
+    z-index: 99;
+
+    .items {
+      display: flex;
+      gap: 50 + px;
+      align-items: center;
+    }
+
+    img {
+      transition: 0.3s ease;
+
+      height: 100px;
+    }
+
+    &.active {
+      transition: 0.3s ease;
+
+      background-color: rgba(0, 0, 0, 0.5);
+
+      backdrop-filter: blur(5px);
+
+      img {
+        transition: 0.3s ease;
+
+        height: 60px;
+      }
+    }
+  }
+</style>
+
+<template></template>
